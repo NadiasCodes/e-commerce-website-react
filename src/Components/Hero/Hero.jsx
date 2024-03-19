55d72d39a401561c061e38267c2d27f391e97a6e
NadiasCodes <De.nadia@outlook.com>
2024-03-19 19:16:37 +0100

    created and styled the hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+import "./Hero.css"
+import clothes_hanger_icon from "../Assets/clothes_hanger_icon.png";
+import arrow from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
+
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+
+<div className="hero-left">
+    <h2>Available Now</h2>
+<div>
+    <div className="hero-clothes-hanger-icon">
+        <p>New</p>
+        <img src={clothes_hanger_icon} alt="hero-icon" />
+    </div>
+    <p>Collections</p>
+    <p>for everyone</p>
+</div>
+<div className="hero-latest-btn">
+    <div>Latest Collection</div>
+    <img src={arrow} alt="arrow"/>
+</div>
+
+
+</div>
+<div className="hero-right">
+
+<img src={hero_image} alt="hero"/>
+</div>
+    </div>
+  )
+}
+
+export default Hero
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero";
 
 export default function Shop() {
   return <div>
-
+<Hero/>
     
   </div>;
 }
