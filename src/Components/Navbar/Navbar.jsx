377d5a5072f1ed5dcb2b96a93c261050994fb550
NadiasCodes <De.nadia@outlook.com>
2024-03-19 16:15:54 +0100

    created an event listener to navigate through the categories

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -11,15 +12,14 @@ const Navbar = () => {
         <p>Velvet</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop <hr/></li>
-        <li>Women</li>
-        <li>Men</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-<button>Login</button>
-<img src={cart_icon} alt="cart-icon" />
-
+        <button>Login</button>
+        <img src={cart_icon} alt="cart-icon" />
       </div>
     </div>
   );
