4a463e543f9b7bbc302e11947eb939838c0f101d
NadiasCodes <De.nadia@outlook.com>
2024-03-20 14:50:43 +0100

    added a new section on the hero page and customized it in CSS

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+
+
+
+
 
 export default function Shop() {
   return <div>
-<Hero/>
+<Hero />
+<Popular />  
     
   </div>;
 }
