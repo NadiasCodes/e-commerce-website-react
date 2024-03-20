8c6830ba4c99e2fe803736112837fa3550a7da5a
NadiasCodes <De.nadia@outlook.com>
2024-03-20 15:38:53 +0100

    replaced an image and then created a banner for the main page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers"
 
 
 
@@ -10,6 +11,7 @@ export default function Shop() {
   return <div>
 <Hero />
 <Popular />  
+<Offers />
     
   </div>;
 }
