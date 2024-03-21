564433afd2415cbb733233bac0f2af7865467c22
NadiasCodes <De.nadia@outlook.com>
2024-03-21 15:06:46 +0100

    created new banners and passed the product data across my categories

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import banner_women from "./Components/Assets/banner_women.png";
+import banner_men from "./Components/Assets/banner_men.png";
+import banner_kids from "./Components/Assets/banner_kids.png";
 
 
 
@@ -17,9 +20,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="women" element={<ShopCategory category="women" />} />
-          <Route path="men" element={<ShopCategory category="men" />} />
-          <Route path="kids" element={<ShopCategory category="kid" />} />
+          <Route path="women" element={<ShopCategory banner={banner_women} category="women" />} />
+          <Route path="men" element={<ShopCategory banner={banner_men} category="men" />} />
+          <Route path="kids" element={<ShopCategory banner={banner_kids} category="kid" />} />
           <Route path="product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
