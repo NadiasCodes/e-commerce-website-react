e0d6cf07fc68542ce7aa21e7d7b29d6d50c3d5c6
NadiasCodes <De.nadia@outlook.com>
2024-03-23 13:36:13 +0100

    added media queries for the navbar for all type of devices

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,32 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 import logo_big from "../Assets/logo_big.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from "../Assets/dropdown_icon.png"
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const{getTotalCartItems}= useContext(ShopContext);
+  const menuRef= useRef();
+  const dropdown_toggle = (e)=>{
+menuRef.current.classList.toggle("nav-menu-visible");
+e.target.classList.toggle("open");
+
+  }
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo_big} alt="logo" />
         <p>Minimal</p>
       </div>
-      <ul className="nav-menu">
-        <li
-          onClick={() => {
-            setMenu("shop");
-          }}
-        >
-          <Link to="/">Shop</Link>
-          {menu === "shop" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("women");
-          }}
-        >
-          <Link to="/women">Women</Link>
-          {menu === "women" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("men");
-          }}
-        >
-          <Link to="/men">Men</Link>
-          {menu === "men" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("kids");
-          }}
-        >
-          <Link to="/kids">Kids</Link>
-          {menu === "kids" ? <hr /> : <></>}
-        </li>
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown}alt="" />
+      <ul ref={menuRef} className="nav-menu">
+        <li onClick={() => {setMenu("shop");}}><Link to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("women");}}><Link to="/women">Women</Link>{menu === "women" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("men");}}><Link to="/men">Men</Link>{menu === "men" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("kids");}}><Link to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <Link to="/login">
